71420c064ec48678c9af9a093ec132da6447fb4d
Manfred Steyer <manfred.steyer@gmx.net>
2024-11-15 23:04:47 +0100

    poc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { loadRemote as loadMfRemote } from '@module-federation/enhanced/runtime';
+import { loadRemoteModule as loadNfRemote } from '@angular-architects/native-federation';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mfruntime';
+
+  async ngOnInit() {
+    await loadMfRemote('react/web-components');
+    const reactElm = document.createElement('react-element');
+    document.body.appendChild(reactElm);
+
+    await loadMfRemote('angular2/web-components');
+    const angular2Elm = document.createElement('angular2-element');
+    document.body.appendChild(angular2Elm);
+
+    await loadMfRemote('angular3/web-components');
+    const angular3Elm = document.createElement('angular3-element');
+    document.body.appendChild(angular3Elm);
+
+    await loadNfRemote('svelte', './web-components');
+    const svelteElm = document.createElement('svelte-mfe');
+    document.body.appendChild(svelteElm);
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { initFederation as initNativeFederation } from '@angular-architects/native-federation';
+import { init as initModuleFederation } from '@module-federation/enhanced/runtime';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+initModuleFederation({
+  name: 'shell',
+  remotes: [
+    {
+      name: 'react',
+      entry: 'https://witty-wave-0a695f710.azurestaticapps.net/remoteEntry.js'
+    },
+    {
+      name: 'angular2',
+      entry: 'https://gray-pond-030798810.azurestaticapps.net/remoteEntry.js'
+    },
+    {
+      name: 'angular3',
+      entry: 'https://gray-river-0b8c23a10.azurestaticapps.net/remoteEntry.js'
+    },
+  ]
+});
+
+initNativeFederation('federation.manifest.json')
+  .catch(err => console.error(err))
+  .then(_ => import('./bootstrap'))
+  .catch(err => console.error(err));
