7b2175c00f4a71cee9525c2650949fa210c2ea50
Manfred Steyer <manfred.steyer@gmx.net>
2024-11-16 14:45:01 +0100

    qed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { loadRemote as loadMfRemote } from '@module-federation/enhanced/runtime';
 import { loadRemoteModule as loadNfRemote } from '@angular-architects/native-federation';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-  title = 'mfruntime';
-
-  async ngOnInit() {
-    await loadMfRemote('react/web-components');
-    const reactElm = document.createElement('react-element');
-    document.body.appendChild(reactElm);
-
-    await loadMfRemote('angular2/web-components');
-    const angular2Elm = document.createElement('angular2-element');
-    document.body.appendChild(angular2Elm);
-
-    await loadMfRemote('angular3/web-components');
-    const angular3Elm = document.createElement('angular3-element');
-    document.body.appendChild(angular3Elm);
-
-    await loadNfRemote('svelte', './web-components');
-    const svelteElm = document.createElement('svelte-mfe');
-    document.body.appendChild(svelteElm);
-  }
-
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,60 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WrapperComponent, WrapperConfig } from './wrapper/wrapper.component';
+import { startsWith } from './shared/starts-with';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        component: HomeComponent,
+    },
+    {
+        path: 'react',
+        component: WrapperComponent,
+        data: {
+            config: {
+                kind: 'module',
+                remoteName: 'react',
+                exposedModule: 'web-components',
+                elementName: 'react-element',
+            } as WrapperConfig
+        }
+    },
+    {
+        path: 'angular2',
+        component: WrapperComponent,
+        data: {
+            config: {
+                kind: 'module',
+                remoteName: 'angular2',
+                exposedModule: 'web-components',
+                elementName: 'angular2-element',
+            } as WrapperConfig
+        }
+    },
+    {
+        matcher: startsWith('angular3'),
+        component: WrapperComponent,
+        data: {
+            config: {
+                kind: 'module',
+                remoteName: 'angular3',
+                exposedModule: 'web-components',
+                elementName: 'angular3-element',
+            } as WrapperConfig
+        }
+    },
+    {
+        path: 'svelte',
+        component: WrapperComponent,
+        data: {
+            config: {
+                kind: 'native',
+                remoteName: 'svelte',
+                exposedModule: './web-components',
+                elementName: 'svelte-mfe',
+            } as WrapperConfig
+        }
+    },
+];
