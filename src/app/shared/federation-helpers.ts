ab291f5fd6fa600298d137353711aa51710a084a
Manfred Steyer <manfred.steyer@gmx.net>
2024-11-17 19:09:30 +0100

    feat: respect multiple version in getShared

--- src/app/shared/federation-helpers.ts
+++ src/app/shared/federation-helpers.ts
@@ -8,16 +8,18 @@ type NativeFederationContainer = {
   };
 };
 
-export type ShareConfig = {
-  [pkgName: string]: {
-    version: string;
-    scope?: string;
-    get: () => Promise<() => Type<unknown>>;
-    shareConfig?: {
-      singleton?: boolean;
-      requiredVersion: string;
-    };
+export type ShareObject = {
+  version: string;
+  scope?: string;
+  get: () => Promise<() => Type<unknown>>;
+  shareConfig?: {
+    singleton?: boolean;
+    requiredVersion: string;
   };
+};
+
+export type ShareConfig = {
+  [pkgName: string]: Array<ShareObject>;
 };
 
 export async function initCrossShare(shared: ShareConfig) {
@@ -33,17 +35,21 @@ export function getShared() {
   const externals = nfc.__NATIVE_FEDERATION__.externals;
   const shared: ShareConfig = {};
 
-  for (const key of externals.keys()) {
-    if (
-      !key.startsWith('/@id/') &&
-      !key.startsWith('@angular-architects/module-federation')
-    ) {
+  const allKeys = [...externals.keys()];
+  const keys = allKeys.filter(k => 
+    !k.startsWith('/@id/')
+    && !k.startsWith('@angular-architects/module-federation')
+    && !k.endsWith('@')
+  )
+  .sort();
+
+  for (const key of keys) {
       const idx = key.lastIndexOf('@');
       const pkgName = key.substring(0, idx);
       const version = key.substring(idx + 1);
       const path = externals.get(key) ?? '';
 
-      shared[pkgName] = {
+      const shareObj: ShareObject = {
         version,
         get: async () => {
           const lib = await (window as any).importShim(path);
@@ -54,7 +60,13 @@ export function getShared() {
           requiredVersion: version,
         },
       };
-    }
+
+      if (!shared[pkgName]) {
+        shared[pkgName] = [];
+      }
+
+      shared[pkgName].push(shareObj);
+    
   }
   return shared;
 }
